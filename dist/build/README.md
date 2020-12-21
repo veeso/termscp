3220d00b1434f161b0f997db638c6ddd6a58cb1b
ChristianVisintin <christian.visintin1997@gmail.com>
2020-12-21 10:27:33 +0100

    archlinux dist

---
+++ dist/build/README.md
@@ -0,0 +1,25 @@
+# Build with Docker
+
+- [Build with Docker](#build-with-docker)
+  - [Prerequisites](#prerequisites)
+  - [Build](#build)
+
+---
+
+## Prerequisites
+
+- Docker
+
+## Build
+
+1. Build x86_64
+
+    this will build termscp for:
+
+      - Linux x86_64 Deb packages
+      - Linux x86_64 RPM packages
+      - Windows x86_64 MSVC packages
+  
+    ```sh
+    
+    ```
--- dist/build/deploy.sh
+++ dist/build/deploy.sh
@@ -7,14 +7,25 @@ fi
 
 VERSION=$1
 
+mkdir -p pkgs/${VERSION}/
 # Build x86_64
 cd x86_64/
 docker build --tag termscp-${VERSION}-x86_64 .
-# Get pkgs
+# Create container and get deb, rpm
 cd -
-# Create container
+mkdir -p pkgs/${VERSION}/deb/
+mkdir -p pkgs/${VERSION}/rpm/
 CONTAINER_NAME=$(docker create termscp-${VERSION}-x86_64 termscp-${VERSION}-x86_64)
-docker cp ${CONTAINER_NAME}:/usr/src/TermSCP/target/debian/termscp_${VERSION}_amd64.deb .
-docker cp ${CONTAINER_NAME}:/usr/src/TermSCP/target/release/rpmbuild/RPMS/x86_64/termscp-${VERSION}-1.x86_64.rpm .
+docker cp ${CONTAINER_NAME}:/usr/src/termscp/target/debian/termscp_${VERSION}_amd64.deb pkgs/${VERSION}/deb/
+docker cp ${CONTAINER_NAME}:/usr/src/termscp/target/release/rpmbuild/RPMS/x86_64/termscp-${VERSION}-1.x86_64.rpm pkgs/${VERSION}/rpm/
+# Build x86_64_archlinux
+cd x86_64_archlinux/
+docker build --tag termscp-${VERSION}-x86_64_archlinux .
+# Create container and get AUR pkg
+cd -
+mkdir -p pkgs/${VERSION}/arch/
+CONTAINER_NAME=$(docker create termscp-${VERSION}-x86_64_archlinux termscp-${VERSION}-x86_64_archlinux)
+docker cp ${CONTAINER_NAME}:/usr/src/termscp/termscp-${VERSION}-x86_64.tar.gz pkgs/arch/
+docker cp ${CONTAINER_NAME}:/usr/src/termscp/PKGBUILD pkgs/${VERSION}/arch/
 
 exit $?
