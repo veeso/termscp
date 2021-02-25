669fd2386830e4ab0be1fbba58012532f05de95d
veeso <christian.visintin1997@gmail.com>
2021-02-25 14:43:27 +0100

    Support for older distributions

--- dist/build/deploy.sh
+++ dist/build/deploy.sh
@@ -12,15 +12,19 @@ cd ..
 PKGS_DIR=$(pwd)/pkgs
 cd -
 mkdir -p ${PKGS_DIR}/
-# Build x86_64
-cd x86_64/
-docker build --tag termscp-${VERSION}-x86_64 .
-# Create container and get deb, rpm
+# Build x86_64_deb
+cd x86_64_debian8/
+docker build --tag termscp-${VERSION}-x86_64_debian8 .
 cd -
 mkdir -p ${PKGS_DIR}/deb/
-mkdir -p ${PKGS_DIR}/rpm/
-CONTAINER_NAME=$(docker create termscp-${VERSION}-x86_64 termscp-${VERSION}-x86_64)
+CONTAINER_NAME=$(docker create termscp-${VERSION}-x86_64_debian8 termscp-${VERSION}-x86_64_debian8)
 docker cp ${CONTAINER_NAME}:/usr/src/termscp/target/debian/termscp_${VERSION}_amd64.deb ${PKGS_DIR}/deb/
+# Build x86_64_centos7
+cd x86_64_centos7/
+docker build --tag termscp-${VERSION}-x86_64_centos7 .
+cd -
+mkdir -p ${PKGS_DIR}/rpm/
+CONTAINER_NAME=$(docker create termscp-${VERSION}-x86_64_centos7 termscp-${VERSION}-x86_64_centos7)
 docker cp ${CONTAINER_NAME}:/usr/src/termscp/target/release/rpmbuild/RPMS/x86_64/termscp-${VERSION}-1.x86_64.rpm ${PKGS_DIR}/rpm/
 # Build x86_64_archlinux
 cd x86_64_archlinux/
