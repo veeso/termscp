35ab9ae202014e183c8443569c52eb6e12da2c0b
veeso <christian.visintin1997@gmail.com>
2021-03-01 20:33:04 +0100

    Added githubActions features to handle github tests; set git fetch test under github actions exclude pattern

--- src/utils/git.rs
+++ src/utils/git.rs
@@ -78,6 +78,7 @@ mod tests {
     use super::*;
 
     #[test]
+    #[cfg(not(all(target_os = "macos", feature = "githubActions")))]
     fn test_utils_git_check_for_updates() {
         assert!(check_for_updates("100.0.0").ok().unwrap().is_none());
         assert!(check_for_updates("0.0.1").ok().unwrap().is_some());
