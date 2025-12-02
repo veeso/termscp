694232564a597546bf241c20815e9756385685b6
veeso <christian.visintin@veeso.dev>
2025-12-02 14:25:55 +0100

    fix: install.sh deb name

--- install.sh
+++ install.sh
@@ -10,8 +10,8 @@
 
 TERMSCP_VERSION="0.19.0"
 GITHUB_URL="https://github.com/veeso/termscp/releases/download/v${TERMSCP_VERSION}"
-DEB_URL_AMD64="${GITHUB_URL}/termscp_${TERMSCP_VERSION}_amd64.deb"
-DEB_URL_AARCH64="${GITHUB_URL}/termscp_${TERMSCP_VERSION}_arm64.deb"
+DEB_URL_AMD64="${GITHUB_URL}/termscp_${TERMSCP_VERSION}-1_amd64.deb"
+DEB_URL_AARCH64="${GITHUB_URL}/termscp_${TERMSCP_VERSION}-1_arm64.deb"
 
 PATH="$PATH:/usr/sbin"
 
