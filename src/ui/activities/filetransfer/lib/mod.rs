b75fb61f43e2c995fd32aabbb26bb5c47e22ac69
Christian Visintin <christian.visintin@veeso.dev>
2026-02-27 16:57:45 +0100

    refactor: add Pane struct for symmetric browser panes

--- src/ui/activities/filetransfer/lib/mod.rs
+++ src/ui/activities/filetransfer/lib/mod.rs
@@ -3,5 +3,6 @@
 //! `filetransfer_activiy` is the module which implements the Filetransfer activity, which is the main activity afterall
 
 pub(crate) mod browser;
+pub(crate) mod pane;
 pub(crate) mod transfer;
 pub(crate) mod walkdir;
