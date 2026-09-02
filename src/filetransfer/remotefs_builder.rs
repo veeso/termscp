622a64ac58d6fbd578de05e32f1433bde8845d61
Christian Visintin <christian.visintin@veeso.dev>
2026-09-02 22:27:56 +0200

    build: bump remotefs-smb 0.5.0

--- src/filetransfer/remotefs_builder.rs
+++ src/filetransfer/remotefs_builder.rs
@@ -12,9 +12,11 @@ use remotefs_gcs::credentials::service_account;
 use remotefs_gcs::{GoogleCloudStorageCredentials, GoogleCloudStorageFs};
 use remotefs_kube::KubeMultiPodFs as KubeFs;
 #[cfg(smb)]
-use remotefs_smb::{SmbCredentials, SmbFs};
+use remotefs_smb::{PavaoSmbCredentials as SmbCredentials, PavaoSmbFs as SmbFs};
 #[cfg(smb_unix)]
-use remotefs_smb::{SmbDialect as RemoteSmbDialect, SmbOptions};
+use remotefs_smb::{PavaoSmbOptions as SmbOptions, SmbDialect as RemoteSmbDialect};
+#[cfg(smb_windows)]
+use remotefs_smb::{WNetSmbCredentials as SmbCredentials, WNetSmbFs as SmbFs};
 use remotefs_ssh::{
     NoCheckServerKey, RusshSession as SshSession, ScpFs, SftpFs, SshAgentIdentity,
     SshConfigParseRule, SshOpts,
